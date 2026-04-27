fffe1db66c7eb5a0fce7b937b0a5b07a41d1c0e5
OwenChadwick <144071219+OwenChadwick@users.noreply.github.com>
2026-04-27 22:18:37 +0000

    not sure what this commit is, just trying to get all up to date

--- Code.js
+++ Code.js
@@ -1,7 +1,7 @@
 /**
  * Main entry point for the web app.
  * Serves the Index.html file.
- * is version 2 even getting there
+ * 
  */
 function doGet() {
   return HtmlService.createTemplateFromFile('Index')
